d1465ec839c55b507b8a5fe8d163cd19c6915103
Ronald Pinheiro <pinheiroronald12@gmail.com>
2024-09-10 09:10:00 -0300

    ajustes

--- scripts/sitema.js
+++ scripts/sitema.js
@@ -6,11 +6,11 @@ function mostrar(){
     let n1 = Number(num1.value)
     if(n1 > 10){
         n1 = Math.floor(Math.random()*10)
-        alert(`Só são aceitos numeros iguais ou menores que 10\nFoi conciderado o numero ${n1}`)
+        alert(`Só são aceitos numeros iguais ou menores que dez\nFoi conciderado o numero ${n1}`)
     }
     if(n1<0){
         n1 = Math.floor(Math.random()*11)
-        alert(`Só são aceitos numeros iguais ou maiores que 0\nFoi conciderado o número ${n1}`)
+        alert(`Só são aceitos números iguais ou maiores que zero\nFoi conciderado o número ${n1}`)
     }
     for(let ia = 1; ia < 11; ia++){
         div = Math.floor(n1 / ia)
